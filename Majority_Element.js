9f3f7cad60b78cd6bf1433b4f119a75cf2326662
LaraStumpp <30356922+LaraStumpp@users.noreply.github.com>
2021-03-19 23:05:55 -0700

    Create Majority_Element.js

---
+++ Majority_Element.js
@@ -0,0 +1,37 @@
+/**
+ * Majority Element
+ * Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ */
+function majorityElement(nums) {
+    var hashmap={};
+    
+    for(var i = 0; i<nums.length; i++){
+        if(nums[i] in hashmap)
+            {
+                hashmap[nums[i]] = hashmap[nums[i]] + 1;
+            } else{
+                hashmap[nums[i]] = 1;
+            }
+      
+    }
+    
+    var majority = nums.length / 2;
+    for(var key in hashmap){
+         if(hashmap[key] > majority){
+            return key;
+        }
+    }
+}
